21ae2807bb638cfd2a665b5c8ffdafb557a739ba
Humayun <151178033+gitFlyy@users.noreply.github.com>
2024-05-05 16:49:19 +0500

    Delete LastLineDefence.cpp

--- LastLineDefence.cpp
+++
@@ -1,9 +0,0 @@
-#include "LastLineDefence.h"
-
-LastLineDefence::LastLineDefence() {
-    for (int i = 0; i < ROWS; i++) {
-        lawnMowers[i] = false;
-        poolCleaners[i] = false;
-        roofCleaners[i] = false;
-    }
-}
